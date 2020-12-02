fcf475b589a5194aec2d772a4216774a55d88d84
İsmail Karaman <ismailkaraman@msn.com>
2020-12-02 21:30:53 +0300

    Sync

---
+++ OgrIsler/OgrIsler.DataAccess/Concrete/EntityFrameWork/OgrBilgiDal.cs
@@ -0,0 +1,39 @@
+﻿using OgrIsler.DataAccess.Abstract;
+using OgrIsler.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrIsler.DataAccess.Concrete.EntityFrameWork
+{
+    public class OgrBilgiDal : IOgrBilgiDal
+    {
+        public void Add(OgrBilgi Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(OgrBilgi Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OgrBilgi Get(Expression<Func<OgrBilgi, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OgrBilgi> GetList(Expression<Func<OgrBilgi, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(OgrBilgi Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
